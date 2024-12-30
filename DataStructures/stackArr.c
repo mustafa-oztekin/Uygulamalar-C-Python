14472434aad9cc3a33e5b67e7dd52c2d4629918f
Mustafa Öztekin <mustafaoztekin564@gmail.com>
2024-12-30 20:49:37 +0300

    dizi kopyalamada fonksiyon kullanımı

--- DataStructures/stackArr.c
+++ DataStructures/stackArr.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "stackArr.h"
 
 // dinamik olarak dizilerin boyutlarını değiştirdik
@@ -22,8 +23,11 @@ int pop(stack *s) {
 
     if(s->top <= s->size / 4 && s->size > 2) {   // dinamik boyut azaltma
         int *dizi2 = (int*)malloc(sizeof(int) * s->size / 2);
-        for(int i = 0; i < s->top; i++)
-            dizi2[i] = s->dizi[i];
+
+        memcpy(dizi2, s->dizi, sizeof(int) * s->top);
+        //for(int i = 0; i < s->top; i++)
+        //    dizi2[i] = s->dizi[i];
+        
         free(s->dizi); //garbage collector, hafızada kullanılmayan alan temizleme, memory management 
         s->dizi = dizi2;
         s->size /= 2;
@@ -40,8 +44,15 @@ void push(int value, stack *s) {
     // bu işlemler çok verimsiz, örnek olarak 1 milyon veri ile çalışırken 2 milyonluk hafıza açıp 1 milyon kopyalama sonra 1 milyon silme yapıyor, bu yüzden hafıza işlmeleri için verimsiz ve çok yavaş kalır büyük veri setleri ile çalışırken
     if(s->top >= s->size - 1) {   // dinamik boyut artırma
         int *dizi2 = (int*)malloc(sizeof(int) * s->size * 2);
-        for(int i = 0; i < s->size; i++)
-            dizi2[i] = s->dizi[i];
+        if(dizi2 == NULL) {
+            printf("Hafiza tahsisi basarisiz!\n");
+            return;
+        }
+
+        memcpy(dizi2, s->dizi, sizeof(int) * s->size);
+        //for(int i = 0; i < s->size; i++)
+        //    dizi2[i] = s->dizi[i];
+
         free(s->dizi); //garbage collector, hafızada kullanılmayan alan temizleme, memory management 
         s->dizi = dizi2;
         s->size *= 2;
